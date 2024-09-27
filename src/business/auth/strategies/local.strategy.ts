09f479c4b0a4d8c9c67840a17a173b1163cb716f
rybakN <dnb4949@gmail.com>
2024-09-27 10:15:40 +0300

    feat: added validate LoginDto

--- src/business/auth/strategies/local.strategy.ts
+++ src/business/auth/strategies/local.strategy.ts
@@ -2,6 +2,9 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from '../services/auth.service';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { LoginDto } from '../dto/login.dto';
+import { validate } from 'class-validator';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -10,10 +13,26 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(username: string, password: string): Promise<any> {
+    const loginDto = plainToClass(LoginDto, {
+      username,
+      password,
+    });
+    const validateErrors = await this.validateLoginDto(loginDto);
+    if (validateErrors) {
+      throw new UnauthorizedException(validateErrors);
+    }
     const user = await this.authService.validateUser({ username, password });
     if (!user) {
       throw new UnauthorizedException('Invalid login or password');
     }
     return user;
+  }
+
+  private async validateLoginDto(loginDto: LoginDto) {
+    const errors = await validate(loginDto);
+    if (errors.length) {
+      return errors.map((error) => error.constraints);
+    }
+    return null;
   }
 }
