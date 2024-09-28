01c20a452e43e99701f0b035329473c004c482c2
rybakN <dnb4949@gmail.com>
2024-09-28 20:44:44 +0300

    feat: added LoggingInterceptor

---
+++ src/core/interceptors/logging.interceptor.ts
@@ -0,0 +1,43 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  HttpStatus,
+  Logger,
+  NestInterceptor,
+} from '@nestjs/common';
+import { catchError, Observable, tap, throwError } from 'rxjs';
+
+export class LoggingInterceptor implements NestInterceptor {
+  private readonly logger = new Logger('HTTP');
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const now = Date.now();
+
+    const req = context.switchToHttp().getRequest();
+    const { url, method, ip } = req;
+
+    this.logger.log(`Incoming request: ${method} ${url} from ${ip}`);
+
+    return next.handle().pipe(
+      tap(() => {
+        const response = context.switchToHttp().getResponse();
+        const { statusCode } = response.statusCode;
+        const contentLength = response.get('content-length');
+
+        this.logger.log(
+          `Outgoing Response: ${method} ${url} ${statusCode} ${contentLength || 0} - ${
+            Date.now() - now
+          }ms`,
+        );
+      }),
+      catchError((err) => {
+        this.logger.error(
+          `Error Response: ${method} ${url} - ${err.status || HttpStatus.INTERNAL_SERVER_ERROR} - ${
+            err.message
+          }`,
+        );
+        return throwError(() => err);
+      }),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './core/filters/all-exceptions.filter';
+import { LoggingInterceptor } from './core/interceptors/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.enableCors();
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalInterceptors(new LoggingInterceptor());
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get<number>('app.port'));
