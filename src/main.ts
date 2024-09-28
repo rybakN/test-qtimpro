11b9773e75ae4604bd365a3c8ecc41c861b5d10c
rybakN <dnb4949@gmail.com>
2024-09-28 17:31:08 +0300

    feat: added allExceptionsFilter

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './core/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.enableCors();
+  app.useGlobalFilters(new AllExceptionsFilter());
+
   const configService = app.get(ConfigService);
-  await app.listen(configService.get('app.port'));
+  await app.listen(configService.get<number>('app.port'));
 }
 bootstrap();
