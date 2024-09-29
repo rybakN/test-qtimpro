ce8d81d34fb4873dcad815901bee193450507106
rybakN <dnb4949@gmail.com>
2024-09-29 12:00:30 +0300

    feat: added openApi document

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './core/filters/all-exceptions.filter';
 import { LoggingInterceptor } from './core/interceptors/logging.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import * as YAML from 'yamljs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +15,22 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new LoggingInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('Articles API')
+    .setDescription(
+      'A comprehensive API documentation for the authentication and articles endpoints.',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  const openApiPath = join(__dirname, '..', 'openApi', 'openapi.yaml');
+  const openApiDocument = YAML.parse(readFileSync(openApiPath, 'utf8'));
+  const mergedDocument = {
+    ...document,
+    ...openApiDocument,
+  };
+  SwaggerModule.setup('api', app, mergedDocument);
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get<number>('app.port'));
